fbb3e18ac5c0e84f747c44d6b34798c74a9ccca8
Hwa-Seon <hwaseon.lecouey@gmail.com>
2020-12-18 18:41:17 +0100

    Rename variables according to python naming convention

--- 02_Password_Philosophy/02.py
+++ 02_Password_Philosophy/02.py
@@ -2,22 +2,22 @@
 
 ## Day 2: Password Philosophy
 
-def countValidPassword():
-    validPasswordCount = 0
+def count_valid_password():
+    valid_password_count = 0
     with open('input.txt') as file:
         for line in file:
             min, max, letter, password = parse(line)
             count = password.count(letter)
             if (count >= min and count <= max):
-                validPasswordCount += 1
-    return validPasswordCount
+                valid_password_count += 1
+    return valid_password_count
 
 def parse(line):
     # example 6-7 z: dqzzzjbzz
     t = line.split(' ')
-    minMax = t[0].split('-')
+    min_max = t[0].split('-')
     letter = t[1][0]
     password = t[2]
-    return int(minMax[0]), int(minMax[1]), letter, password
+    return int(min_max[0]), int(min_max[1]), letter, password
 
-print(countValidPassword())
+print(count_valid_password())
