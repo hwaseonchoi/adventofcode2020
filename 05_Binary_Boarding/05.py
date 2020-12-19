cf1c1462c25e33670a03729836136f64d834886a
Hwa-Seon <hwaseon.lecouey@gmail.com>
2020-12-19 21:08:28 +0100

    Complete day 5

--- 05_Binary_Boarding/05.py
+++ 05_Binary_Boarding/05.py
@@ -11,6 +11,22 @@ def highest_seat():
                 id_max = id
         return id_max
 
+def find_my_seat(input):
+    seats = []
+    with open(input) as file:
+        for line in file:
+            seats.append(convert(line.strip()))
+
+    seats.sort()
+    previous_seat = None
+    for seat in seats:
+        if previous_seat is None:
+            previous_seat = seat
+            continue
+        if seat - previous_seat > 1:
+            return seat - 1
+        previous_seat = seat
+
 def convert(s):
     row_string = s[0:7]
     column_string = s[7:10]
@@ -19,5 +35,12 @@ def convert(s):
 
     return row*8+col
 
-print(convert('BBBBBBBRRR')) #test
-print(highest_seat())
+#test
+def test_converter():
+    assert convert('BBBBBBBRRR') == 1023
+
+def test_resolve():
+    print('convert BBBBBBBRRR:', convert('BBBBBBBRRR'))
+    print('highest seat', highest_seat())
+    print('my seat', find_my_seat('input.txt'))
+    assert False, 'solutions'
